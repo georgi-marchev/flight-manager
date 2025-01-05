6b4881877154adebd6a4117bb5c165dd00855436
georgi-marchev <george_85_bg@yahoo.com>
2025-01-05 14:36:58 +0200

    Configure build to place static files in the spring project resources/static dir. Exclude the java project resources/static from git.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../backend/src/main/resources/static', // Output to Spring Boot's static directory
+    emptyOutDir: true, // Ensure the output directory is cleared before each build
+    assetsDir: '', // Specify where to place assets (will be same dir as index.html)
+  },
+  base: './',
 })
